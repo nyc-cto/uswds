817f4ede258cd867ab93687e96e1a1ce4e4a1a11
John Stendardo <johnakasquib96@gmail.com>
2020-07-31 13:15:49 -0400

    Changed attributes, added comments No longer returning a template literal, now returning a usual HTML5 button. Common attributes all buttons need were added, and attributes and functions that have to do with event handling were removed. Added comments that are mostly from the MDN documentation for each attribute (except for class).

--- jsx-templates/button.jsx
+++ jsx-templates/button.jsx
@@ -1,29 +1,47 @@
-//this template will live on USWDS repo
-//--sandbox for styles.css--//
-//https://codesandbox.io/s/bold-voice-xwend?file=/src/App.js
-
-exports.buttonFunc = function (config) {
-  return `
-    //functions
-    function hoverOn(e) {
-      e.target.style.background = "green";
-    }
-    function hoverOff(e) {
-      e.target.style.background = "white";
-    }
-    function focus(e) {
-      e.target.style.background = "red";
-    }
-    
-      <Button
-        onClick = {${config.onClick ? config.onClick : false}}
-        onMouseEnter = {${config.onMouseEnter ? config.onMouseEnter : false}}
-        onMouseLeave = {${config.onMouseLeave ? config.onMouseLeave : false}}
-        onFocus = {${config.onFocus ? config.onFocus : false}}
-        inverse = {${config.inverse ? config.inverse : false}}
-        disabled = {${config.disabled ? config.disabled : false}}
-        outline = {${config.outline ? config.outline : false}}
-        unstyled = {${config.unstyled ? config.unstyled : false}}
-  
-      />`;
+{
+  /* 
+  ATTRIBUTES FOR THE BUTTON
+  ******************************
+  class
+  This will specify what CSS the button will get from USWDS' CSS files.
+  The base class is usa-button with additions coming from the config.class property passed in
+  ----------------------------
+  autofocus 
+  Comes from HTML5
+  This Boolean attribute specifies that the button should have input focus when the page loads. 
+  Only one element in a document can have this attribute. 
+  ----------------------------
+  disabled
+  This Boolean attribute prevents the user from interacting with the button: it cannot be pressed or focused.
+  Firefox, unlike other browsers, persists the dynamic disabled state of a <button> across page loads.
+  Use the autocomplete attribute to control this feature.
+  ----------------------------
+  name
+  The name of the button, submitted as a pair with the button’s value as part of the form data.
+  ----------------------------
+  type
+  The default behavior of the button. Possible values are:
+  submit: The button submits the form data to the server.
+  This is the default if the attribute is not specified for buttons associated with a <form>, or if the attribute is an empty or invalid value.
+  reset: The button resets all the controls to their initial values, like <input type="reset">. (This behavior tends to annoy users.)
+  button: The button has no default behavior, and does nothing when pressed by default. 
+  It can have client-side scripts listen to the element's events, which are triggered when the events occur.
+  ----------------------------
+  value
+  Defines the value associated with the button’s name when it’s submitted with the form data. This value is passed to the server in params when the form is submitted.
+  */
+}
+exports.render = function (config) {
+  return (
+    <button
+      class={`uswds-button ${config.class}`}
+      autofocus={config.autofocus}
+      disabled={config.disabled}
+      name={config.name}
+      type={config.type}
+      value={config.value}
+    >
+      {config.innerText}
+    </button>
+  );
 };
