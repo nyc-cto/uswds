ea1e44cf3dc737dbee523c8a9f2abdc91de9b86c
Mathew Davidov <41017026+MathewDavidov@users.noreply.github.com>
2020-08-10 11:31:21 -0400

    Update button.jsx with consistent JSDoc
    * Format existing and updated documentation
    * Update documentation English
    * Add HTML list to type

--- src/components/02-buttons/button.jsx
+++ src/components/02-buttons/button.jsx
@@ -1,80 +1,41 @@
-{
-  /**  
-  ATTRIBUTES FOR THE BUTTON
-  ******************************
-  @type { Function } Button
-  @param {Object} config [description]  
-  @property {string} class
-   - This will specify what CSS the button will get from USWDS' CSS files.
-  The base class is usa-button with additions coming from the config.class property passed in
-  ----------------------------
-  @property {boolean} autofocus 
-   - This Boolean attribute specifies that the button should have input focus when the page loads. 
-  Only one element(button) in a document can have this attribute. 
-  ----------------------------
-  @property {boolean} disabled
-   - Boolean attribute that prevents the user from interacting with a button i.e.(cannot click on the button).
-  ----------------------------
-  @property {string} name      - The name of the button.
-  ----------------------------
-  @property {string} type      - 
-  submit: The button submits the form data to the server (default type if not specifed). 
-  button: Used for events (onclick, onfocus, onhover)
-  reset: reset controls to their initial values
-  ----------------------------
-  @property {string} value
-   - Defines the value associated with the button’s name when it’s submitted with the form data. This value is passed to the server in params when the form is submitted.
-  ------------------------------
-  @property {string} innerText  - The text to displayed on the button 
-  Setting Up Config 
-  ******************************
-  single class usage
-  @example 
-  const config = {
-    class: "usa-button--secondary",
-    autofocus: "true",  //boolean value only
-    disabled: "false",  //boolean value only
-    name: "Button",
-    type: "submit",
-    value: "",
-    innerText ="Button"
-  }
-  @returns
-  <button
-      class="usa-button usa-button--secondary"
-      autofocus=true
-      disabled=true
-      name="Button"
-      type="submit"
-      value=""
-    >
-      Button
-    </button>
--------------------------------------
-  multiple class usage
-  @example
-    const config = {
-    class: "usa-button--outline usa-button--inverse",
-    autofocus: "true",  //boolean value only
-    disabled: "false",  //boolean value only
-    name: "Button",
-    type: "submit",
-    value: "",
-    innerText ="Button"
-  }
-  @returns
-  <button
-      class="usa-button usa-button--outline usa-button--inverse"
-      autofocus=true
-      disabled=true
-      name="Button"
-      type="submit"
-      value=""
-    >
-      Button
-    </button>
-  */
-}
+/**
+ * JSX Button Component Attributes
+ * @function Button
+ * @param {Object} config The configuration object for button attributes
+ * @property {string} config.class      - The classes inherited from the USWDS CSS files. The default class is `usa-button` and additions can be appended.
+ * @property {boolean} config.autofocus - The attribute that specifies whether or not the button has input focus when the page loads. Only one element (button) in a document can have this attribute.
+ * @property {boolean} config.disabled  - The attribute that prevents the user from interacting with a button, i.e. cannot click on the button.
+ * @property {string} config.name       - The name of the button.
+ * @property {string} config.type       - The type. Three types:
+ *                                        <ul>1. Submit: The button submits the form data to the server (default type if not specifed).</ul>
+ *                                        <ul>2. Button: Used for events (onclick, onfocus, onhover).</ul>
+ *                                        <ul>3. Reset: Reset controls to their initial values.</ul>
+ * @property {string} config.value      - The value associated with the button’s name when it’s submitted with the form data.
+ * @property {string} config.innerText  - The text to displayed on the button.
+ * @example
+ *   // Single class usage
+ *   const config = {
+ *     class: "usa-button--secondary",
+ *     autofocus: "true", //boolean value only
+ *     disabled: "false", //boolean value only
+ *     name: "Button",
+ *     type: "submit",
+ *     value: "",
+ *     innerText: "Button",
+ *   }
+ * @example
+ *   // Multiple class usage
+ *   const config = {
+ *      class: "usa-button--outline usa-button--inverse",
+ *      autofocus: "true", //boolean value only
+ *      disabled: "false", //boolean value only
+ *      name: "Button",
+ *      type: "submit",
+ *      value: "",
+ *      innerText: "Button",
+ *   }
+ * @returns {HTMLButtonElement} The JSX HTML button
+ */
 exports.Button = function(config) {
   return (
     <button
