9e2caf165170889d68b0376a91e803329f608ad3
JohnAKASquib <johnakasquib96@gmail.com>
2020-08-14 10:34:43 -0400

    Updated alert.jsx Put the logic from the extra function inside of the Alerts function, renamed alert.jsx to alerts.jsx and renamed function to Alerts to stay consistent with the .njk files

--- src/components/05-alerts/alerts.jsx
+++ src/components/05-alerts/alerts.jsx
@@ -17,11 +17,21 @@
  *      headerText: null
  *      innerText: "Note: Some aspects of the site are a work in progress and may have some bugs. We appreciate your patience."
  * }
- * @returns {HTMLDivElement} The JSX HTML Alert element
+ * @returns {HTMLDivElement} The HTML Alert element
  */
 
-exports.Alert = function (config) {
-  theHeader = alertHeader(config);
+exports.Alerts = function (config) {
+  let theHeader;
+  if (
+    //these 2 CSS classes aren't supposed to have a header
+    config.class.includes("usa-alert--slim") ||
+    config.class.includes("usa-alert--no-icon")
+  ) {
+    theHeader = null;
+  } else {
+    //any other class can have a header
+    theHeader = <h3 class="usa-alert__heading">{config.headerText}</h3>;
+  }
   return (
     <div class={`usa-alert ${config.class}`}>
       ::before
@@ -31,22 +41,4 @@ exports.Alert = function (config) {
       </div>
     </div>
   );
-};
-/**
- * @function alertHeader
- * @param {Object} config - The configuration object for alert attributes
- * @property {string} config.headerText - The text to be displayed as the header for the alert, if its supposed to have one.
- * @returns {HTMLHeadElement} - The header that may or may not be in the alert element
- */
-let alertHeader = function (config) {
-  if (
-    //these 2 CSS classes aren't supposed to have a header
-    config.class.includes("usa-alert--slim") ||
-    config.class.includes("usa-alert--no-icon")
-  ) {
-    return;
-  } else {
-    //any other class can have a header
-    return <h3 class="usa-alert__heading">{config.headerText}</h3>;
-  }
 };
