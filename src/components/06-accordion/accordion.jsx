55ec7492ca5f1d80fec5effad7b0537b7f20bdb5
JohnAKASquib <johnakasquib96@gmail.com>
2020-08-18 10:03:10 -0400

    accordion inner elements have their own function

--- src/components/06-accordion/accordion.jsx
+++ src/components/06-accordion/accordion.jsx
@@ -33,30 +33,12 @@
  *   Accordion(config);
  * @returns {Accordion} The Accordion
  */
-exports.Accordion = function(config) {
+exports.Accordion = function (config) {
   const variant = config.variant ? `usa-accordion--${config.variant}` : "";
   const itemArray = [];
 
   for (let i = 0; i < config.items.length; i += 1) {
-    itemArray.push(
-      <>
-        <h2 class="usa-accordion__heading">
-          <button
-            class="usa-accordion__button"
-            aria-expanded={config.items[i].expanded}
-            aria-controls={`${config.id_prefix}${config.items[i].id}`}
-          >
-            {config.items[i].title}
-          </button>
-        </h2>
-        <div
-          id={`${config.id_prefix}${config.items[i].id}`}
-          class="usa-accordion__content usa-prose"
-        >
-          {config.items[i].content}
-        </div>
-      </>
-    );
+    itemArray.push(AccordionItem(config.id_prefix, config.items[i]));
   }
 
   return (
@@ -66,5 +48,44 @@ exports.Accordion = function(config) {
     >
       {itemArray}
     </div>
+  );
+};
+/**
+ * JSX Accordion Item Component Attributes
+ * @function AccordionItem
+ * @param {string} id_prefix - The ID prefix for the entire accordion
+ * @param {Object} itemConfig - Configuration object containing all properties for an accordion item
+ * @property {boolean} itemConfig.expanded  - The boolean that determines if the item is expanded
+ * @property {string} itemConfig.id         - The ID of an item
+ * @property {string} itemConfig.title      - The title of an item
+ * @property {string} itemConfig.content    - The content of an item
+ * @example
+ *    const itemConfig={
+ *      expanded: false,
+ *      id: "1",
+ *      title: "Example",
+ *      content: "This is just an example of what could be inside of an accordion item"
+ * }
+ * AccordionItem ("a", itemConfig);
+ */
+exports.AccordionItem = function (id_prefix, itemConfig) {
+  return (
+    <>
+      <h2 class="usa-accordion__heading">
+        <button
+          class="usa-accordion__button"
+          aria-expanded={itemConfig.expanded}
+          aria-controls={`${id_prefix}${itemConfig.id}`}
+        >
+          {itemConfig.title}
+        </button>
+      </h2>
+      <div
+        id={`${id_prefix}${itemConfig.id}`}
+        class="usa-accordion__content usa-prose"
+      >
+        <p> {itemConfig.content}</p>
+      </div>
+    </>
   );
 };
